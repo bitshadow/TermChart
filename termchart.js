c5428fadab62f9ca050b916707028dd61273ea87
Jignesh Kakadiya <jigneshhk1992@gmail.com>
2013-07-31 23:09:11 +0530

    extend termchart to behave as emitter

--- termchart.js
+++ termchart.js
@@ -4,14 +4,18 @@
  */
 var Canvas = require('term-canvas')
     , cp = require('child_process')
+    , EventEmitter = require('events').EventEmitter
+    , util = require('util')
     , path = require('path')
     , exec = cp.exec
     , spawn = cp.spawn;
 
-var canvas = new Canvas(50, 50)
+//process.stdout.getWindowSize()
+var canvas = new Canvas(100, 100)
   , ctx = canvas.getContext('2d')
   , max_limit = 10;
 
+module.exports = TermChart;
 
 process.on('SIGINT', function(){
   ctx.reset();
@@ -20,21 +24,27 @@ process.on('SIGINT', function(){
   });
 });
 
-
 ctx.hideCursor();
 
-function getStats(line)
+function TermChart() {
+    EventEmitter.apply(this);
+}
+
+util.inherits(TermChart, EventEmitter);
+TermChart.prototype.max_limit = function(limit){
+    this.max_limit = limit;
+}
+
+TermChart.prototype.draw = function(arr)
 {
-    var arr = line.split(" ");
-    console.log(arr);
     ctx.clearRect(0,0,canvas.width, canvas.height);
     x = 3;
     arr.forEach(function(n){
-        var y = 20
+        var y = 30
           , w = 5
-          , h = 20*(n/max_limit);
+          , h = 30*(n/max_limit);
 
-        ctx.fillStyle = 'blue';
+        ctx.fillStyle = 'white';
         ctx.fillRect(x, y-h+3, w, h);
         ctx.fillStyle = 'black';
         ctx.fillText(n.toString(), x+2, y+1);
@@ -42,16 +52,17 @@ function getStats(line)
     });
 }
 
-function watchLog(filename) {
+TermChart.prototype.trackLog = function(filename) {
     var fp = path.resolve(process.cwd(), filename);
-    this.fname = fp;
-    this.tail = spawn('tail', ['-F', this.fname]);
+    this.filename = fp;
+    this.tail = spawn('tail', ['-F', this.filename]);
     this.tail.stdout.setEncoding('utf8');
     var lines = require('lines');
     lines(this.tail.stdout);
+    termchart = this;
 
     this.tail.stdout.on('line', function(line){
-        getStats(line);
+        termchart.draw(line.split(" "));
     });
 
     this.tail.once('exit', function(code, signal){
@@ -63,4 +74,6 @@ function watchLog(filename) {
     });*/
 }
 
-watchLog("log.txt");
+
+var t = new TermChart();
+t.trackLog("log.txt");
