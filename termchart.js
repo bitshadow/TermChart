eb8c6ecc53e2e838db49d9047ed18ef6d20e143c
Jignesh Kakadiya <jigneshhk1992@gmail.com>
2013-07-31 23:30:17 +0530

    refactor

--- termchart.js
+++ termchart.js
@@ -7,16 +7,15 @@ var Canvas = require('term-canvas')
     , EventEmitter = require('events').EventEmitter
     , util = require('util')
     , path = require('path')
-    , exec = cp.exec
     , spawn = cp.spawn;
 
 //process.stdout.getWindowSize()
 var canvas = new Canvas(100, 100)
-  , ctx = canvas.getContext('2d')
-  , max_limit = 10;
+  , ctx = canvas.getContext('2d');
 
 module.exports = TermChart;
 
+//Clear canvas when recieved terminate signal like [^C]
 process.on('SIGINT', function(){
   ctx.reset();
   process.nextTick(function(){
@@ -24,25 +23,38 @@ process.on('SIGINT', function(){
   });
 });
 
+// hide curser from context
 ctx.hideCursor();
 
+/*
+ * Our termchart object inherits from eventemitter
+ */
 function TermChart() {
     EventEmitter.apply(this);
 }
-
 util.inherits(TermChart, EventEmitter);
+
+/*
+ *  max limit of chart
+ *  @param limit
+ */
 TermChart.prototype.max_limit = function(limit){
     this.max_limit = limit;
 }
 
+/*
+ *  Draw the given data array into the canvas
+ *  @param arr contains the last line of the file
+ */
 TermChart.prototype.draw = function(arr)
 {
     ctx.clearRect(0,0,canvas.width, canvas.height);
     x = 3;
+    termchart = this;
     arr.forEach(function(n){
         var y = 30
           , w = 5
-          , h = 30*(n/max_limit);
+          , h = 30*(n/termchart.max_limit);
 
         ctx.fillStyle = 'white';
         ctx.fillRect(x, y-h+3, w, h);
@@ -52,28 +64,46 @@ TermChart.prototype.draw = function(arr)
     });
 }
 
-TermChart.prototype.trackLog = function(filename) {
-    var fp = path.resolve(process.cwd(), filename);
-    this.filename = fp;
+/**
+ *  Execute tail as child process.
+ *  Generate for every single line appended
+ *  @param absolute path of the file
+ */
+TermChart.prototype.execTail = function(filename){
+    this.filename = filename;
+
     this.tail = spawn('tail', ['-F', this.filename]);
     this.tail.stdout.setEncoding('utf8');
     var lines = require('lines');
     lines(this.tail.stdout);
     termchart = this;
-
     this.tail.stdout.on('line', function(line){
         termchart.draw(line.split(" "));
     });
 
     this.tail.once('exit', function(code, signal){
-        console.log("exit");
+        termchart.emit('error', new Error("tail subprocess ended badly"));
+        termchart.emit('endtermchart');
     });
-
-   /* this.on('end', function(){
-        console.log("end");
-    });*/
 }
 
+/**
+ *  Track file. if data is appended the generate
+ *  event to update the chart.
+ *  @param filename
+ */
+TermChart.prototype.trackLog = function(filename) {
+    var fp = path.resolve(process.cwd(), filename);
+    this.execTail(fp);
+    this.on('endtermchart', function(){
+        this.tail.kill();
+    })
+}
+
+TermChart.prototype.close = function(){
+    this.emit('endtermchart');
+}
 
 var t = new TermChart();
+t.max_limit(10);
 t.trackLog("log.txt");
