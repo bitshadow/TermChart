8d8102bd8c6c159f5b5fdff64b16bc365aaffa41
Jignesh Kakadiya <jigneshhk1992@gmail.com>
2013-08-01 11:24:07 +0530

    display using live logs

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 var TermChart = require('./termchart')
-
+    , fs = require('fs');
 
 var t = new TermChart();
 t.max_limit(10);
 t.trackLog("log.txt");
+
+
+function rand() {
+    return Math.random() * t.max_limit | 0;
+}
+
+setInterval(function(){
+  var data = [rand(), rand(), rand(), rand(), rand(), rand()];
+  var log = data.join(' ') + "\n";
+  console.log(data);
+  fs.appendFile('log.txt', log, function (err) {
+        if (err) throw err;
+  });
+}, 1000);
